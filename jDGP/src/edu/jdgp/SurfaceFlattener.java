58e2fb851d3e568601106a726ec397dba423b3ea
manudubinsky@gmail.com <manudubinsky@gmail.com@d7b4e731-a48f-7d5a-b8d2-2f8ccfe1badd>
2014-11-20 19:57:13 +0000

    Empezando a implementar flattening. El DecoratedPolygonMesh calcula las normas de los ejes y las normales a las caras, el SurfaceFlattener es el que aplana una supeficie

---
+++ jDGP/src/edu/jdgp/SurfaceFlattener.java
@@ -0,0 +1,21 @@
+package edu.jdgp;
+
+import edu.jdgp.DGP.PolygonMesh;
+
+public class SurfaceFlattener {
+	DecoratedPolygonMesh _surface;
+	
+	public SurfaceFlattener(PolygonMesh surface) {
+		_surface = new DecoratedPolygonMesh(surface);
+	}
+	
+	/*
+	 *  devuelve una version aplanada de la superfice (ie. los angulos entre 
+	 *  las normales con ~= 0 y las normas de los ejes son similares)  
+	 */
+	
+	public PolygonMesh flatten(){
+	
+		return null;
+	}
+}
